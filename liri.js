b45e8c7630c2c25cd8799057bf0191795bec5289
s1191 <danieljudsonblair@gmail.com>
2019-01-18 23:17:37 -0500

    concert-this function complete (with warning)

--- liri.js
+++ liri.js
@@ -1,35 +1,29 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
+var axios = require("axios");
+var moment = require('moment');
 // var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
-var artist = process.argv[3];
-var movie = artist;
+var nodeArgs = process.argv;
+var fullName = "";
 
-
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        fullName = fullName + "+" + nodeArgs[i];
+    }
+    else {
+        fullName += nodeArgs[i];
+    }
+}
 
 
 var movieF = function () {
-    var axios = require("axios");
-    var nodeArgs = process.argv;
-    var movieName = "";
-
-    for (var i = 3; i < nodeArgs.length; i++) {
-
-        if (i > 3 && i < nodeArgs.length) {
-            movieName = movieName + "+" + nodeArgs[i];
-        }
-        else {
-            movieName += nodeArgs[i];
-        }
-    }
-    if (movieName === "") {
-        movieName = "Mr. Nobody";
+    if (fullName === "") {
+        fullName = "Mr. Nobody";
     }
 
-    let queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    let queryUrl = "http://www.omdbapi.com/?t=" + fullName + "&y=&plot=short&apikey=trilogy";
 
     axios.get(queryUrl).then(
         function (response) {
@@ -41,9 +35,35 @@ var movieF = function () {
             console.log("Language: " + response.data.Language);
             console.log("Plot: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
-        }
-    );
+        })
+    // fs.appendFile(".txt", ", " + num, function (err) {
+    //     if (err) {
+    //         console.log(err);
+    //     }
 
+};
+
+var concertF = function () {
+
+    let queryUrl = "https://rest.bandsintown.com/artists/" + fullName +
+        "/events?app_id=codingbootcamp"
+
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(moment());
+            for (let i = 0; i < response.data.length; i++) {
+                console.log("Venue: " + response.data[i].venue.name);
+                if (response.data[i].venue.country === 'United States') {
+                    console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+                    console.log("Date: " + moment(response.data[i].datetime).format('L'));
+                } else {
+                    console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+                    console.log("Date: " + moment(response.data[i].datetime).format('L'));
+                }
+
+            }
+            // console.log(response.data[0]);
+        })
 };
 
 switch (command) {
